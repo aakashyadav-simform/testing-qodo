5d03cc5159c7608d971352283f35d0ba76365756
Aakash Yadav <aakash.yadav@simformsolutions.com>
2026-01-21 15:51:56 +0530

    new_testing

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     a = int(os.getenv("A", "0"))
     b = int(os.getenv("B", "0"))
     result = add(a, b)
-    print(f"The sum of {a} and {b} is {result} testing.")
+    print(f"This is the result: {result}")
 
