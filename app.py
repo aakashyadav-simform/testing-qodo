bdd7c3f784c6d5a71057475f3b44cad22e535282
Aakash Yadav <aakash.yadav@simformsolutions.com>
2026-01-21 15:53:46 +0530

    Change output message on main branch

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     a = int(os.getenv("A", "0"))
     b = int(os.getenv("B", "0"))
     result = add(a, b)
-    print(f"This is the result: {result}")
+    print(f"Result from MAIN branch: {result}")
 
